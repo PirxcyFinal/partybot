2d5f0352c96cb898b16e85f03af3eee32eefea57
PirxcyFinal <71592833+PirxcyFinal@users.noreply.github.com>
2021-05-21 07:50:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ else:
   sys.exit()
 
 os.system('pip install certifi')
-os.system('pip install -U PartyBotPackage')
+os.system('pip install GummyFN --upgrade')
 os.system('clear')
 
 import uvloop
